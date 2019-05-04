b8bc0ac0fc707cecf983ceb456333c2c9b555cba
scientistcoco <courtney.lum@gmail.com>
2019-05-04 18:11:11 +1000

    Updates

---
+++ precache-manifest.8aabe3614cb869e2240010a83db594b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b28882763502a53d72450e0258d2d9",
+    "url": "/Tic-Tac-Toe-Minimax/index.html"
+  },
+  {
+    "revision": "2d2894846c863c3ba47c",
+    "url": "/Tic-Tac-Toe-Minimax/static/css/main.60f595f0.chunk.css"
+  },
+  {
+    "revision": "92e6a3189caa6d422d24",
+    "url": "/Tic-Tac-Toe-Minimax/static/js/2.dc687d74.chunk.js"
+  },
+  {
+    "revision": "2d2894846c863c3ba47c",
+    "url": "/Tic-Tac-Toe-Minimax/static/js/main.6fd763d6.chunk.js"
+  },
+  {
+    "revision": "7c2d034d68f4d3a15392",
+    "url": "/Tic-Tac-Toe-Minimax/static/js/runtime~main.addebd75.js"
+  }
+]);
